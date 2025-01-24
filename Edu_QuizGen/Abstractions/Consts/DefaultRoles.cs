99a7e91be9214217092610aaebd965068a1b0211
Rafek <rafeknaim@gamil.com>
2025-01-24 23:43:02 +0200

    Add Roles

---
+++ Edu_QuizGen/Abstractions/Consts/DefaultRoles.cs
@@ -0,0 +1,18 @@
+﻿namespace Edu_QuizGen.Abstractions.Consts;
+
+public static class DefaultRoles
+{
+    public static string Admin =nameof(Admin);
+    public static string AdminRoleId = "0194895d-f050-7461-b24f-89bd825b3846";
+    public static string AdminRoleConcurrencyStamp = "01948960-9b3e-7104-bd12-5083222a2c8a";
+
+
+    public static string Student = nameof(Student);
+    public static string StudentRoleId = "0194895d-f050-7461-b24f-89be04b4f5ad";
+    public static string StudentRoleConcurrencyStamp = "01948960-9b3e-7104-bd12-5084e4273ede";
+
+
+    public static string Teacher = nameof(Teacher);
+    public static string TeacherRoleId = "0194895d-f050-7461-b24f-89bf1364a1ba";
+    public static string TeacherRoleConcurrencyStamp = "01948960-9b3e-7104-bd12-508568b47c8c";
+}
---
+++ Edu_QuizGen/Abstractions/Consts/DefaultUsers.cs
@@ -0,0 +1,11 @@
+﻿namespace Edu_QuizGen.Abstractions.Consts;
+
+public class DefaultUsers
+{
+    public const string AdminId = "019488ef-b9bd-7b5c-adeb-997f2443d22b";
+    public const string AdminEmail = "admin@GuizGen.com";
+    public const string AdminPassword = "Admin123@";
+    public const string SecurityStamp = "GE49A7A12DC0F4459930840D718724172";
+    public const string ConcurrencyStamp = "019488f4-5437-7bd0-88a4-491c73dde6ea";
+    
+}

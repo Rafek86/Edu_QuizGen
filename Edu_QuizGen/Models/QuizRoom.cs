f8e1ff99859b612e48246c5e87d7271f5bdf30e9
Rafek <rafeknaim@gamil.com>
2025-02-26 23:00:08 +0200

    CreateDbMonster

--- Edu_QuizGen/Models/QuizRoom.cs
+++ Edu_QuizGen/Models/QuizRoom.cs
@@ -3,7 +3,7 @@
     public class QuizRoom
     {
         public int QuizId { get; set; }
-        public int RoomId { get; set; }
+        public string RoomId { get; set; }
         public Room Room { get; set; }
         public Quiz Quiz { get; set; }
     }

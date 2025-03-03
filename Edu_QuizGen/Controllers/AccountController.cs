a175934c33a4ec649fe1d1b51fa7924f43c4998f
Rafek <rafeknaim@gamil.com>
2025-03-03 17:30:10 +0200

    RemoveAuthorizationForNow

--- Edu_QuizGen/Controllers/AccountController.cs
+++ Edu_QuizGen/Controllers/AccountController.cs
@@ -7,31 +7,30 @@ using SurveyBasket.Extensions;
 namespace Edu_QuizGen.Controllers;
 [Route("me")]
 [ApiController]
-[Authorize]
+//[Authorize]
 public class AccountController(IUserService userService) : ControllerBase
 {
     private readonly IUserService _userService = userService;
 
-    [HttpGet("")]
-    public async Task<IActionResult> Info() {
-
-        var result = await _userService.GetProfileAsync(User.GetUserId());
-        
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> Info([FromRoute] string Id) 
+    {
+        var result = await _userService.GetProfileAsync(Id); 
         return Ok(result.Value);
     }
 
-    [HttpPut("info")]
-    public async Task<IActionResult> Info([FromBody] UpdateProfileRequest request) {
+    [HttpPut("info/{Id}")]
+    public async Task<IActionResult> Info([FromRoute] string Id,[FromBody] UpdateProfileRequest request) {
 
-        await _userService.UpdateProfileAsync(User.GetUserId(),request);
+        await _userService.UpdateProfileAsync(Id,request);
 
         return NoContent();
     }
     
-    [HttpPut("change-password")]
-    public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordRequest request) {
+    [HttpPut("change-password/{Id}")]
+    public async Task<IActionResult> ChangePassword([FromRoute] string Id,[FromBody] ChangePasswordRequest request) {
 
-        var result = await _userService.ChangePasswordAsync(User.GetUserId(),request);
+        var result = await _userService.ChangePasswordAsync(Id,request);
 
        return result.IsSuccess ? NoContent()
             : result.ToProblem();

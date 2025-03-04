c1b64625a36f06172da3b97d06cb8d89ec3d83ec
Mena Safwat Adolf Zaki <mena21-01070@student.eelu.edu.eg>
2025-03-04 08:18:19 +0200

    repo

---
+++ Edu_QuizGen/Repository/GenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using Edu_QuizGen.Repository_Abstraction;
+
+namespace Edu_QuizGen.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbContext.Set<T>().ToListAsync();
+        
+        public async Task<T> GetByIdAsync(string id) => await _dbContext.Set<T>().FindAsync(id);
+        public async Task<T> GetByIdAsync(int id) => await _dbContext.Set<T>().FindAsync(id);
+    }
+}
---
+++ Edu_QuizGen/Repository_Abstraction/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Edu_QuizGen.Repository_Abstraction
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        
+        Task<T> GetByIdAsync(string id); //for (student, teacher, room, hash)
+        Task<T> GetByIdAsync(int id); //for other classes
+    }
+}

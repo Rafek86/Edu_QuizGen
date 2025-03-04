9846ad309ca36e798400ee6c04fb82b2408fa12a
Mena Safwat Adolf Zaki <mena21-01070@student.eelu.edu.eg>
2025-03-04 10:09:03 +0200

    mig

---
+++ Edu_QuizGen/Migrations/20250304080613_ststusToIsDeleted.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Edu_QuizGen.Migrations
+{
+    /// <inheritdoc />
+    public partial class ststusToIsDeleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Rooms",
+                newName: "IsDisabled");
+
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Quiz",
+                newName: "IsDisabled");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDisabled",
+                table: "Rooms",
+                newName: "Status");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDisabled",
+                table: "Quiz",
+                newName: "Status");
+        }
+    }
+}
